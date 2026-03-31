0349e501046e3bb6d2b27fa7af45c502ef587062
sanjeev <avalasanjeev03@gmail.com>
2026-03-31 15:20:58 +0530

    Add divide

--- src/main/java/org/example/Calculation.java
+++ src/main/java/org/example/Calculation.java
@@ -2,22 +2,12 @@ package org.example;
 
 public class Calculation {
 
-    public int sum(int a, int b) {
-        return a + b;
-    }
-
-    //average
-    public double average(int a, int b) {
-        int sum = a + b;
-        return (double)sum / 2;
-    }
-
-    public void printMessage() {
-        int a = 0;
-        System.out.println("hello!!");
-    }
-
     public int divide(int a, int b) {
-        return a/b;
+        return a / b; // possible division by zero
+    }
+
+    public void nullPointer() {
+        String s = null;
+        System.out.println(s.length()); // NPE
     }
 }
